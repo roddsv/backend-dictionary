b06f595b6e3ec499c3b15d276de4d98426c48853
Rodrigo Soares <adsrodsoares@gmail.com>
2024-05-31 16:12:09 -0300

    Criação de rota '/'

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import pool from './infrastructure/database/db';
+const express = require('express');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
-async function testConnection() {
-  try {
-    const client = await pool.connect();
-    const res = await client.query('SELECT * FROM test');
-    console.log(res.rows[0]);
-    client.release();
-  } catch (err) {
-    console.error('Error connecting to the database:', err);
-  } finally {
-    pool.end();
-  }
-}
+const app = express();
 
-testConnection();
+const port: Number = 3001
+
+app.use(cors());
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.get('/', (req: Request, res: Response) =>
+  console.log('Test')
+)
+
+app.listen(port, () => {
+  console.log('API is running!')
+})
